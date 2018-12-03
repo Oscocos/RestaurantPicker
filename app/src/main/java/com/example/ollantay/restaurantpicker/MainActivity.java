5bbee233d493b9959d302a18b570021162941a04
Oscocos <olscocos@gmail.com>
2018-12-03 16:47:03 -0600

    Added settings activity.

--- app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
+++ app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.ollantay.restaurantpicker;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton spinButton;
+    private ImageButton settingsButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinButton = (ImageButton) findViewById(R.id.spin_button);
+
+
+        settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsMenu();
+            }
+        });
+
     }
+
+    public void openSettingsMenu() {
+         Intent intent = new Intent(this, SettingsMenu.class);
+         startActivity(intent);
+    }
+
+    /*ImageButton spinButton = (ImageButton) findViewById(R.id.spin_button);
+
+    spinButton.OnClickListener(new View.OnClickListener()) {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, SettingsMenu.class));
+        }
+    }*/
 }
