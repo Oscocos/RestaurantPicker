3e55d910433cb542d28b2f152c4f81c07066d879
Oscocos <olscocos@gmail.com>
2018-12-07 11:38:32 -0600

    Started work on API functions

--- app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
+++ app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.ollantay.restaurantpicker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
     private ImageButton spinButton;
     private ImageButton settingsButton;
+
+    private static final int GRANTED_LOCATION_REQUEST = 0;
+    private View snackBarLayout;
 
 
 
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         spinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                pickARestaurant();
                 openSpinResult();
             }
         });
@@ -38,6 +47,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void pickARestaurant() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(snackBarLayout, "Location available, picking restaurant...", Snackbar.LENGTH_SHORT).show();
+            pick();
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    public void pick() {
+
+    }
+
+    public void requestLocationPermission() {
+
+    }
+
     public void openSpinResult() {
         Intent intent  = new Intent(this, SpinResult.class);
         startActivity(intent);
@@ -48,12 +74,4 @@ public class MainActivity extends AppCompatActivity {
          startActivity(intent);
     }
 
-    /*ImageButton spinButton = (ImageButton) findViewById(R.id.spin_button);
-
-    spinButton.OnClickListener(new View.OnClickListener()) {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, SettingsMenu.class));
-        }
-    }*/
 }
