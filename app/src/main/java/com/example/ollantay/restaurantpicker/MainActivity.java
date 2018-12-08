77beda6f2ef1f10c92c5d52cc0eebe6b235a4ec8
Oscocos <olscocos@gmail.com>
2018-12-08 15:05:46 -0600

    App now requests access to location
      Need to get and store Lat and Long

--- app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
+++ app/src/main/java/com/example/ollantay/restaurantpicker/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.ollantay.restaurantpicker;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     private ImageButton spinButton;
     private ImageButton settingsButton;
 
-    private static final int GRANTED_LOCATION_REQUEST = 0;
-    private View snackBarLayout;
+    // Locatin request code
+    private static final int GRANTED_LOCATION_REQUEST = 1;
 
 
 
@@ -31,8 +36,13 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         spinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pickARestaurant();
-                openSpinResult();
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //pickARestaurant();
+                    openSpinResult();
+                } else {
+                    requestLocationPermission();
+                }
             }
         });
 
@@ -48,12 +58,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     }
 
     public void pickARestaurant() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Snackbar.make(snackBarLayout, "Location available, picking restaurant...", Snackbar.LENGTH_SHORT).show();
-            pick();
-        } else {
-            //requestLocationPermission();
-        }
+
     }
 
     public void pick() {
@@ -61,7 +66,37 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     }
 
     public void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Neeeded")
+                    .setMessage("Location needed for the app to work at all!")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, GRANTED_LOCATION_REQUEST);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, GRANTED_LOCATION_REQUEST);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == GRANTED_LOCATION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void openSpinResult() {
