bbb61c019cf3a14e738e086df861c0cfa29d4ab3
Oscocos <olscocos@gmail.com>
2018-12-11 17:27:43 -0600

    almost finished picking restaurant.

--- app/src/main/java/com/example/ollantay/restaurantpicker/SettingsMenu.java
+++ app/src/main/java/com/example/ollantay/restaurantpicker/SettingsMenu.java
@@ -2,8 +2,11 @@ package com.example.ollantay.restaurantpicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingsMenu extends AppCompatActivity {
+
+    public Switch delivery = (Switch) findViewById(R.id.deliverySwitch);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
